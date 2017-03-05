6f0614a460bba35442e032acd03650e1c02ccc45
lipeng <leer.li@ian-soft.com>
2017-03-05 21:05:57 +0800

    add event handlers

--- client/application/home/konva-stage/index.vue
+++ client/application/home/konva-stage/index.vue
@@ -11,8 +11,11 @@
 						height: 768
 					})
 					let layer = new Konva.Layer()
+
+					// 添加背景图
 					let imageObj = new Image()
-					imageObj.onload = function() {
+					imageObj.src = img
+					imageObj.onload = function () {
 						let yoda = new Konva.Image({
 							x: 0,
 							y: 0,
@@ -23,7 +26,30 @@
 						layer.add(yoda)
 						stage.add(layer)
 					}
-					imageObj.src = img
+
+					// 获取长宽函数
+					let getSize = (pointStart, pointEnd) => {
+						return {
+							width: pointEnd.x - pointStart.x,
+							height: pointEnd.y - pointStart.y
+						}
+					}
+                    
+
+					// 监听事件
+					stage.on('mousedown', () => {
+						let pointStart = stage.getPointerPosition()
+						stage.on('mousemove', () => {
+							let pointEnd = stage.getPointerPosition()
+							console.log('usual mousemove ' + JSON.stringify(stage.getPointerPosition()))
+						})
+					})
+
+					stage.on('mouseup', () => {
+						console.log('usual mouseup ' + JSON.stringify(stage.getPointerPosition()))
+						stage.off('mousemove')
+					})
+
 				}
 			}
 		}
