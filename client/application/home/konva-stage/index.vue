138de7dcb41a6def01bcbc5cfdf0edf7978f3776
lipeng <leer.li@ian-soft.com>
2017-03-10 16:08:14 +0800

    add remark

--- client/application/home/konva-stage/index.vue
+++ client/application/home/konva-stage/index.vue
@@ -1,7 +1,7 @@
 <script>
 	import Konva from '../../../plugins/konva/konva-map'
 	import coverList from '../cover-list'
-    import coverType from '../cover-type'
+	import coverType from '../cover-type'
 	export default {
 		// 实例挂载后的生命周期，不保证组件已在document中
 		mounted(){
@@ -16,19 +16,20 @@
 		},
 		methods: {
 			// 删除图形
-			deleteFromCovers:function (cover) {
-                this.deleteCover(cover)
-                this.konva.deleteCover(cover)
+			deleteFromCovers: function (cover) {
+				this.deleteCover(cover)
+				this.konva.deleteCover(cover)
 			},
 			// 选择图形类型
-			selectCoverType:function (type) {
+			selectCoverType: function (type) {
 				this.selectType(type)
 				this.konva.graphicType = type
+				this.konva.remark = prompt("请输入简单备注", "例如儿童商店一号厅")
 			},
 			...Vuex.mapActions('home', [
 				'outputCover',
-                'deleteCover',
-                'selectType'
+				'deleteCover',
+				'selectType'
 			])
 		},
 		components: {
@@ -43,10 +44,10 @@
     <div class="row">
         <div class="col-10">
             <div id="konva-stage"></div>
-            <cover-list v-on:delete = 'deleteFromCovers'></cover-list>
+            <cover-list v-on:delete='deleteFromCovers'></cover-list>
         </div>
         <div class="col-2">
-            <cover-type v-on:selectType = 'selectCoverType'></cover-type>
+            <cover-type v-on:selectType='selectCoverType'></cover-type>
         </div>
     </div>
 </template>
